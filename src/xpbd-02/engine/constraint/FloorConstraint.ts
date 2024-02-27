27306da778b4bfc05a4ec4d805d3f4a20926a163
richardkolisek <richard.kolisek@eskerpartners.com>
2024-02-27 21:40:50 +0100

    * dummy floor friction

--- src/xpbd-02/engine/constraint/FloorConstraint.ts
+++ src/xpbd-02/engine/constraint/FloorConstraint.ts
@@ -1,5 +1,5 @@
 import {PointsData} from "../Points";
-import {POINT_DIAMETER} from "../Constants";
+import {FRICTION, POINT_DIAMETER} from "../Constants";
 
 export const WORLD_MIN_X = -20;
 export const WORLD_MAX_X = 120;
@@ -9,20 +9,33 @@ export const WORLD_MAX_Y = 100;
 
 export const solveFloor = (points: PointsData, index: number) =>
 {
+    // TODO: fix friction
     if (points.positionCurrent[index * 2 + 1] < WORLD_MIN_Y + POINT_DIAMETER)
     {
         points.positionCurrent[index * 2 + 1] = WORLD_MIN_Y + POINT_DIAMETER;
+
+        const diffX = (points.positionCurrent[index * 2] - points.positionPrevious[index * 2]) * FRICTION;
+        points.positionCurrent[index * 2] = points.positionCurrent[index * 2] - diffX;
     }
     else if (points.positionCurrent[index * 2 + 1] > WORLD_MAX_Y - POINT_DIAMETER)
     {
         points.positionCurrent[index * 2 + 1] = WORLD_MAX_Y - POINT_DIAMETER;
+
+        const diffX = (points.positionCurrent[index * 2] - points.positionPrevious[index * 2]) * FRICTION;
+        points.positionCurrent[index * 2] = points.positionCurrent[index * 2] - diffX;
     }
     if (points.positionCurrent[index * 2] < WORLD_MIN_X + POINT_DIAMETER)
     {
         points.positionCurrent[index * 2] = WORLD_MIN_X + POINT_DIAMETER;
+
+        const diffX = (points.positionCurrent[index * 2 + 1] - points.positionPrevious[index * 2 + 1]) * FRICTION;
+        points.positionCurrent[index * 2 + 1] = points.positionCurrent[index * 2 + 1] - diffX;
     }
     else if (points.positionCurrent[index * 2] > WORLD_MAX_X - POINT_DIAMETER)
     {
         points.positionCurrent[index * 2] = WORLD_MAX_X - POINT_DIAMETER;
+
+        const diffX = (points.positionCurrent[index * 2 + 1] - points.positionPrevious[index * 2 + 1]) * FRICTION;
+        points.positionCurrent[index * 2 + 1] = points.positionCurrent[index * 2 + 1] - diffX;
     }
 }
